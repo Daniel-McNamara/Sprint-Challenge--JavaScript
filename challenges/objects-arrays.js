51120ca086a3748bdc382da7a72f44cd44d4d045
daniel mcnamara <danmac8407@gmail.com>
2019-12-20 11:34:05 -0600

    finished all dino objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,6 +13,7 @@ const tyra = {
   weight: '7000kg',
   length: '12m',
   period: 'Late Cretaceous',
+  roar: 'RAWERSRARARWERSARARARRRR!',
 }
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 const stego = {
@@ -33,20 +34,20 @@ const velo = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyra.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyra.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyra.roar);
 
 
 // ==== Arrays ====
